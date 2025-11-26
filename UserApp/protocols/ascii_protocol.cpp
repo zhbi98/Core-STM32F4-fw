4b5e8db39c82b78977a299e3c8c16a9da4f8168f
zhbi98 <1523022952@qq.com>
2025-11-26 15:22:43 +0800

    修复串口命令操作无响应问题

--- UserApp/protocols/ascii_protocol.cpp
+++ UserApp/protocols/ascii_protocol.cpp
@@ -99,7 +99,7 @@ void OnUart2AsciiCmd(const char* _cmd, size_t _len, StreamSink &_responseChannel
             Respond(_responseChannel, "Set command mode to [%lu]", mode);
         } else
             Respond(_responseChannel, "ok");
-    } else if (_cmd[0] == '>' || _cmd[0] == '@')
+    } else if (_cmd[0] == '&'/*'>'*/ || _cmd[0] == '@')
     {
         uint32_t freeSize = dummy.commandHandler.Push(_cmd);
         Respond(_responseChannel, "%d", freeSize);
